39979b1b15c42c1380073921ca364de59f5a4f8f
Augie <augie3v3r3tt@gmail.com>
2023-11-20 23:29:15 +0900

    Main Menu basic button functions, now able to load scenes

---
+++ Assets/Scripts/Managers/MainMenuManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class MainMenuManager : MonoBehaviour
+{
+    /*
+        // World 1 level names
+        [Header("=====World1=====")]
+        [SerializeField] string _world1_level1;
+        [SerializeField] string _world1_level2;
+        [SerializeField] string _world1_level3;
+        [Header("=====World2=====")]
+        // World 2 level names
+        [SerializeField] string _world2_level1;
+        [SerializeField] string _world2_level2;
+        [SerializeField] string _world2_level3;
+    */
+
+    public void LoadLevel(string _levelName)
+    {
+        SceneManager.LoadScene(_levelName);
+    }
+}
