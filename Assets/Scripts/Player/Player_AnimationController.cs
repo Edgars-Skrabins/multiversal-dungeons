d30a75a9731149384cb0ab2a10ba32c35f3bedae
Edgars Skrabiņš <shiftkiro@gmail.com>
2023-11-21 18:53:54 +0200

    Presets for sprite importing, enemy templates

--- Assets/Scripts/Player/Player_AnimationController.cs
+++ Assets/Scripts/Player/Player_AnimationController.cs
@@ -1,18 +1,9 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Player_AnimationController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private Animator m_playerAnimator;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
+    
 }
