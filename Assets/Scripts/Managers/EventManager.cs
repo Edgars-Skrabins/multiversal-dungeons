8449be7482776ae41c53ed22638cdb1e0fa846b4
Edgars Skrabiņš <shiftkiro@gmail.com>
2023-11-21 16:16:12 +0200

    Weapon template logic added

--- Assets/Scripts/Managers/EventManager.cs
+++ Assets/Scripts/Managers/EventManager.cs
@@ -1,10 +1,12 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class EventManager : Singleton<EventManager>
 {
-    
-    
+
+    public event Action OnPlayerInteraction;
+    public event Action OnRoomCleared;
 
 }
--- Assets/Scripts/Templates/Weapon.cs
+++ Assets/Scripts/Templates/Weapon.cs
@@ -1,4 +1,3 @@
-using System;
 using UnityEngine;
 
 public abstract class Weapon : MonoBehaviour
@@ -6,27 +5,57 @@ public abstract class Weapon : MonoBehaviour
 
     [Header("Weapon Settings")]
     [Space(5)]
-    protected float m_fireRateInSeconds;
+    [SerializeField] protected float m_fireRateInSeconds;
+    [SerializeField] protected Transform[] m_shootLocations;
+    [Space(5)]
+    
     protected float m_fireRateTimer;
+    protected bool m_fireRateZero;
 
     protected virtual void Awake()
     {
         SusbcribeToInputEvents();
     }
-
+    
     protected virtual void HandleShoot()
     {
-        
+        if(CanShoot()) Shoot();
     }
 
     protected virtual void Shoot()
     {
-        throw new System.NotImplementedException();
+        ResetFireRateTimer();
+    }
+
+    protected virtual bool CanShoot()
+    {
+        return m_fireRateZero && !IsShootLocationObstructed();
+    }
+
+    protected virtual bool IsShootLocationObstructed()
+    {
+        foreach(Transform tf in m_shootLocations)
+        {
+            // TODO: Make the logic for checking if fire location obstructed
+        }
+        return false;
     }
 
     protected virtual void CountFireRateTimer()
     {
-        throw new System.NotImplementedException();
+        if(m_fireRateTimer >= 0)
+        {
+            m_fireRateTimer -= Time.deltaTime;
+        }
+        else
+        {
+            m_fireRateZero = true;
+        }
+    }
+
+    protected virtual void ResetFireRateTimer()
+    {
+        m_fireRateTimer = m_fireRateInSeconds;
     }
     
     protected virtual void SusbcribeToInputEvents()
