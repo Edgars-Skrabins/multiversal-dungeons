c2fa53ea4c2aca61e32ae7e8afbb64d32bb6ac8c
Augie <augie3v3r3tt@gmail.com>
2023-11-26 12:16:24 +0900

    created more managers, setup scripts,
    TODO: need shoot action, so we can test, moving between rooms, levels, etc... need to defeat enemies to do that.

--- Assets/Scripts/Managers/EnemyManager.cs
+++ Assets/Scripts/Managers/EnemyManager.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class EnemyManager : MonoBehaviour
 {
+
+    [SerializeField] GameObject _enemyType_1;
+    [SerializeField] GameObject _enemyType_2;
+    [SerializeField] GameObject _enemyType_3;
     // Start is called before the first frame update
     void Start()
     {
--- Assets/Scripts/Managers/RoomManager.cs
+++ Assets/Scripts/Managers/RoomManager.cs
@@ -2,13 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SaveManager : MonoBehaviour
+public class RoomManager : MonoBehaviour
 {
-    
-    // player settings
-    // acheievments
-    // score
-    // comleted levels
+    [SerializeField] Transform[] _enemySpawnPoints;
+    [SerializeField] Transform _portalSpawnPoint;
+
     // Start is called before the first frame update
     void Start()
     {
