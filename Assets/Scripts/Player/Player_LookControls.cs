adfd1807ac86eb690cb463ec8937a8cf29643ada
indcrediBro <152566488+indcrediBro@users.noreply.github.com>
2023-12-03 01:12:57 +0530

    Player Look Controls Script

--- Assets/Scripts/Player/Player_LookControls.cs
+++ Assets/Scripts/Player/Player_LookControls.cs
@@ -1,7 +1,41 @@
-using System;
+using MultiversalDungeons.Utilities;
 using UnityEngine;
 
 public class Player_LookControls : MonoBehaviour
 {
+    [SerializeField] private Transform m_virtualCameraTF;
+    [SerializeField] private Transform m_mainCameraTF;
+    [SerializeField] private Transform m_cameraTargetTF;
+    [SerializeField] private float m_threshold;
+
+    private void Awake()
+    {
+        //Utilities.Initialize();
+        UnparentCamerasAndTarget();
+    }
+
+    private void UnparentCamerasAndTarget()
+    {
+        m_virtualCameraTF.SetParent(null);
+        m_mainCameraTF.SetParent(null);
+        //m_cameraTargetTF.SetParent(null);
+    }
+
+    private void Update()
+    {
+        MoveCameraTarget();
+    }
+
+    private void MoveCameraTarget()
+    {
+        Vector3 mousePos = InputManager.I.GetWorldMousePosition();
+        Vector3 targetPosition = (transform.position + mousePos) / 2;
+
+        targetPosition.x = Mathf.Clamp(targetPosition.x, -m_threshold + transform.position.x, m_threshold + transform.position.x);
+        targetPosition.y = Mathf.Clamp(targetPosition.y, -m_threshold + transform.position.y, m_threshold + transform.position.y);
+
+        m_cameraTargetTF.position = targetPosition;
+    }
+
 
 }
