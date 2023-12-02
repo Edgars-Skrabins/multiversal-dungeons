2dc836bcd7af115f2806321c2fafc4dfa0c8dcef
indcrediBro <152566488+indcrediBro@users.noreply.github.com>
2023-12-03 03:12:25 +0530

    Updated player prefab

--- Assets/Scripts/Templates/Pickup_Weapon.cs
+++ Assets/Scripts/Templates/Pickup_Weapon.cs
@@ -2,11 +2,16 @@ using UnityEngine;
 
 public class Pickup_Weapon : Pickup
 {
-    //Use if not picking up Random Weapon
+    /// <summary>
+    /// Leave Weapon Name Blank To Give Random Weapon
+    /// </summary>
     [SerializeField] private string m_weaponName;
     
     protected override void PickUpItem(Player_Stats _playerStatsCS)
     {
-        _playerStatsCS.m_playerWM.EquipWeapon(_playerStatsCS.m_playerWM.GetRandomWeaponName());
+        if(m_weaponName == "")
+            _playerStatsCS.m_playerWM.EquipWeapon(_playerStatsCS.m_playerWM.GetRandomWeaponName());
+        else
+            _playerStatsCS.m_playerWM.EquipWeapon(m_weaponName);
     }
 }
