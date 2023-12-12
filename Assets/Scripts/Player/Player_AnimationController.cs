7c075dba18966c6af6f7f03dd5c9a29259d2e610
indcrediBro <152566488+indcrediBro@users.noreply.github.com>
2023-12-12 18:51:10 +0530

    Player Idle directional Animation

--- Assets/Scripts/Player/Player_AnimationController.cs
+++ Assets/Scripts/Player/Player_AnimationController.cs
@@ -4,6 +4,69 @@ public class Player_AnimationController : MonoBehaviour
 {
     [SerializeField] private Animator m_playerAnimator;
 
-    
-    
+
+    private void Update()
+    {
+        SetFacingDirection();
+    }
+
+    void SetFacingDirection()
+    {
+        Vector2 mousePosition = InputManager.I.GetWorldMousePosition();
+        Vector2 direction = mousePosition - (Vector2)transform.position;
+        direction.Normalize();
+
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+
+        float horizontal = 0f;
+        float vertical = 0f;
+
+        // Determine the facing direction based on the angle
+        if (angle >= 0f && angle <= 45f)
+        {
+            // Up Right
+            horizontal = 1f; 
+            vertical = 1f;
+        }
+        else if (angle > 45f && angle <= 135f)
+        {
+            // Up
+            vertical = 1f; 
+            horizontal = 0f;
+        }
+        else if (angle > 135f && angle <= 180f)
+        {
+            // Up Left
+            vertical = 1f;
+            horizontal = -1f;
+        }
+
+        else if(angle < 0f && angle >= -45)
+        {
+            // Down Right
+            horizontal = 1f;
+            vertical = -1f;
+        }
+        else if (angle < -45f && angle >= -135f)
+        {
+            // Down
+            vertical = -1f;
+            horizontal = 0f;
+        }
+        else if (angle < -135f && angle >= -180f)
+        {
+            // Down Left
+            vertical = -1f;
+            horizontal = -1f;
+        }
+        Debug.Log("Direction = " + angle);
+        // Update animator parameters for facing direction only
+        m_playerAnimator.SetFloat("Horizontal", horizontal);
+        m_playerAnimator.SetFloat("Vertical", vertical);
+    }
+
+    private void SetSpeed()
+    {
+        m_playerAnimator.SetFloat("Speed", InputManager.I.GetMovementVector2Normalized().magnitude);
+    }
 }
