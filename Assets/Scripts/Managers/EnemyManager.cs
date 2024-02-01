c28d5266158d58f725d0d4da09065ddf843a1cca
Augie <augie3v3r3tt@gmail.com>
2024-02-01 20:59:22 +0900

    fixed and added spawn points

--- Assets/Scripts/Managers/EnemyManager.cs
+++ Assets/Scripts/Managers/EnemyManager.cs
@@ -15,30 +15,20 @@ public class EnemyManager : MonoBehaviour
 
     public void SpawnEnemy(int _numberOfEnemies)
     {
-
         Transform[] _possibleSpawnPoints = m_levelManagerSC.m_currentRoom.GetComponent<RoomManager>().m_EnemySpawnPoints;
+        List<Transform> m_spawnPoint = new List<Transform>();
 
-        for (int i = 0; i < _possibleSpawnPoints.Length; i++)
+        for (int i = 0; i < _numberOfEnemies; i++)
         {
-            m_SpawnPointTF = _possibleSpawnPoints[i];
-            Instantiate(m_enemyType_1GO, m_SpawnPointTF);
+            int _randomNumber = Random.Range(0, _possibleSpawnPoints.Length - 1);
+            Debug.Log(_randomNumber);
+            if (!m_spawnPoint.Contains(_possibleSpawnPoints[_randomNumber]))
+            {
+                m_SpawnPointTF = _possibleSpawnPoints[_randomNumber];
+                m_spawnPoint.Add(_possibleSpawnPoints[_randomNumber]);
+                Instantiate(m_enemyType_1GO, m_SpawnPointTF);
+            }
         }
-
-
-        //Transform[] _possibleSpawnPoints = m_levelManagerSC.m_currentRoom.GetComponent<RoomManager>().m_EnemySpawnPoints;
-        //List<Transform> m_spawnPoint = new List<Transform>();
-
-        //for (int i = 0; i < _numberOfEnemies; i++)
-        //{
-        //    int _randomNumber = Random.Range(0, _possibleSpawnPoints.Length - 1);
-        //    Debug.Log(_randomNumber);
-        //    if (!m_spawnPoint.Contains(_possibleSpawnPoints[_randomNumber]))
-        //    {
-        //        m_SpawnPointTF = _possibleSpawnPoints[_randomNumber];
-        //        m_spawnPoint.Add(_possibleSpawnPoints[_randomNumber]);
-        //        Instantiate(m_enemyType_1GO, m_SpawnPointTF);
-        //    }
-        //}
     }
 
 }
